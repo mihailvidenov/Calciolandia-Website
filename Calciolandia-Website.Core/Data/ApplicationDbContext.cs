92c02f5b13b85df1c9a8b9b0fcff73c72743307d
Home <Home@DESKTOP-RNBGJ5V>
2023-07-26 18:34:04 +0300

    Add project files.

---
+++ Calciolandia-Website.Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calciolandia_Website.Core.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
